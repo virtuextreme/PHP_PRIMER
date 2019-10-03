f16211c9b75a404aa02d18f99ecf8c2196832e16
virtuextreme <55068693+virtuextreme@users.noreply.github.com>
2019-10-03 02:42:37 -0500

    Added two more buttons

--- index.php
+++ index.php
@@ -35,4 +35,8 @@
     
     ?>
     <button type="button" class="btn btn-dark">Click Me</button>
+    <button type="button" class="btn btn-primary">Click Me</button>
+    <button type="button" class="btn btn-success">Click Me</button>
+
+
    <?php require 'includes/footer.php' ?>
