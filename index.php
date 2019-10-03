cb0f51097da7b373027487eb6709b390f468ec6d
virtuextreme <55068693+virtuextreme@users.noreply.github.com>
2019-10-03 03:07:09 -0500

    added link to heroku

--- index.php
+++ index.php
@@ -37,6 +37,6 @@
     <button type="button" class="btn btn-dark">Click Me</button>
     <button type="button" class="btn btn-primary">Click Me</button>
     <button type="button" class="btn btn-success">Click Me</button>
-
+    <a href="https://www.heroku.com" target= "_blank"class="btn btn-danger">Heroko.com</a>
 
    <?php require 'includes/footer.php' ?>
